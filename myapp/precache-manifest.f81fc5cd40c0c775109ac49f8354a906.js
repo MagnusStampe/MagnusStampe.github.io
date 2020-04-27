92f031c4fb0d3a3fc21c7f80b1bdb59e95095cdf
Magnus stampe Nielsen <m_s_nielsen288@hotmail.com>
2020-04-27 10:23:30 +0200

    auto-commit

---
+++ myapp/precache-manifest.f81fc5cd40c0c775109ac49f8354a906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d539dd2ffb4936b6789564c799301b4",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "3dcf3a22275191ace2f2",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a7094dbf960e82d7f548",
+    "url": "/myapp/static/js/2.bbca583d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.bbca583d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dcf3a22275191ace2f2",
+    "url": "/myapp/static/js/main.57503ca9.chunk.js"
+  },
+  {
+    "revision": "65c868e4ad6c68fa7a68",
+    "url": "/myapp/static/js/runtime-main.68b876f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
